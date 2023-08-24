7cf2c81ef1a5f0905497fb2a3243b9afb94ab2be
hanyiseo2 <shyshy81818181@gmail.com>
2023-08-25 01:46:18 +0900

    detach app server execution

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import express, { Request, Response } from "express"
+import app from "./app"
 
 const PORT = 3000
-const app = express()
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("HELLO WORLD")
-})
 
 app.listen(PORT, () => {
     console.log(`The server is listening to the port ${PORT}`)
