5ccf308baf9637c8ea46ddcffb26395e4b100450
hanyiseo2 <shyshy81818181@gmail.com>
2023-08-25 00:15:00 +0900

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!")
+import express, { Request, Response } from "express"
+
+const PORT = 3000
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("HELLO WORLD")
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is listening to the port ${PORT}`)
+})
