1b021277744962ced6e1248bd6837a2be1bfa378
hanyiseo2 <shyshy81818181@gmail.com>
2023-09-01 17:40:40 +0900

    add resize function

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,14 +2,18 @@ import { Request, Response } from "express"
 import axios from "axios"
 import { globalConfig } from "./config"
 
-export async function getRandomUnsplashImageUrl(): Promise<string> {
+export async function getRandomUnsplashImageUrl(width: number, height: number): Promise<string> {
     const randomImageUrl = 
         `${globalConfig.unsplash.apiBaseUrl}/photos/random?client_id=${globalConfig.unsplash.apiAccessKey}`
     const { data } = await axios.get(randomImageUrl)
-    return data.urls.full
+    let url = data.urls.full
+    if(isNaN(width) || isNaN(height)) return url
+    return `${url}${width ? `&w=${width}` : ""}${height ? `&h=${height}` : ""}`
 }
 
-export async function getRandomImage(req: Request, res: Response)  {
-    const randomImageUrl = await getRandomUnsplashImageUrl()
+export async function getRandomImage(req: Request, res: Response) {
+    // localhost:3000/?width=200&height=300
+    const { width, height } = req.query
+    const randomImageUrl = await getRandomUnsplashImageUrl(Number(width), Number(height))
     res.redirect(randomImageUrl)
 }
