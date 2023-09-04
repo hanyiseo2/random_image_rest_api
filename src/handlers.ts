9851d247eaa7996056412571d0ab123515fe6f1c
hanyiseo2 <shyshy81818181@gmail.com>
2023-09-05 01:14:06 +0900

    add handlers for search with resize and color

--- src/handlers.ts
+++ src/handlers.ts
@@ -3,17 +3,47 @@ import axios from "axios"
 import { globalConfig } from "./config"
 
 export async function getRandomUnsplashImageUrl(width: number, height: number): Promise<string> {
+    // localhost:3000/?width=200&height=300
     const randomImageUrl = 
         `${globalConfig.unsplash.apiBaseUrl}/photos/random?client_id=${globalConfig.unsplash.apiAccessKey}`
     const { data } = await axios.get(randomImageUrl)
     let url = data.urls.full
-    if(isNaN(width) || isNaN(height)) return url
-    return `${url}${width ? `&w=${width}` : ""}${height ? `&h=${height}` : ""}`
+
+    const imageUrl = checkWidthAndHeight(width,height,url)
+    return imageUrl
 }
 
 export async function getRandomImage(req: Request, res: Response) {
-    // localhost:3000/?width=200&height=300
-    const { width, height } = req.query
+    const {width,height} =req.query
     const randomImageUrl = await getRandomUnsplashImageUrl(Number(width), Number(height))
     res.redirect(randomImageUrl)
+}
+
+export async function getRandomSearchImageUrl(keyword:string, color:string, width:number, height:number): Promise<string>{
+    // localhost:3000/search/:keyword
+    let randomSearchImageUrl = 
+        `${globalConfig.unsplash.apiBaseUrl}/search/photos?query=${keyword}&client_id=${globalConfig.unsplash.apiAccessKey}`
+    if(color){
+        randomSearchImageUrl = 
+        `${globalConfig.unsplash.apiBaseUrl}/search/photos?query=${keyword}&color=${color}&client_id=${globalConfig.unsplash.apiAccessKey}`
+    }
+    
+    const { data } = await axios.get(randomSearchImageUrl)
+    let randomNumber = Math.floor(Math.random() * 10)
+    let url = data.results[randomNumber].urls.full
+    
+    const imageUrl = checkWidthAndHeight(width,height,url)
+    return imageUrl
+}
+
+export async function getRandomSearchImage(req:Request, res:Response){
+    const { keyword} = req.params
+    const {width, height, color}= req.query
+    const randomSearchImageUrl = await getRandomSearchImageUrl(keyword as string, color as string, Number(width), Number(height))
+    res.redirect(randomSearchImageUrl)
+}
+
+export function checkWidthAndHeight(width:number, height:number, url:string){
+    if(isNaN(width) || isNaN(height)) return url
+    return (`${url}${width ? `&w=${width}` : ""}${height ? `&h=${height}` : ""}`)
 }
