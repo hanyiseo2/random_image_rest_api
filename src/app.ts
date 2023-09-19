5b8e23e80536e845e50c8e0299619920d53eaf21
hanyiseo2 <shyshy81818181@gmail.com>
2023-09-19 18:19:21 +0900

    initialize config in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { getRandomImage, getRandomSearchImage } from "./handlers";
 
+import { init } from "./config";
+init();
+
 const app = express();
 
 app.get("/", getRandomImage);
--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,33 @@
-import { config } from "dotenv"
+import { config } from "dotenv";
 
-if(process.env.NODE_ENV === "local") {
-    config({ path: "./.env.local" })
+if (process.env.NODE_ENV === "local") {
+  config({ path: "./.env.local" });
 } else if (process.env.NODE_ENV === "prod") {
-    config({ path: "./.env.prod" })
+  config({ path: "./.env.prod" });
+} else if (process.env.NODE_ENV === "test") {
+  config({ path: "./.env.test" });
 }
 
 type GlobalConfig = {
-    unsplash: {
-        apiBaseUrl: string
-        apiAccessKey: string
-    }
-}
+  unsplash: {
+    apiBaseUrl: string;
+    apiAccessKey: string;
+  };
+};
 
 export let globalConfig: GlobalConfig = {
-    unsplash: {
-        apiAccessKey: "",
-        apiBaseUrl: ""
-    }
-}
+  unsplash: {
+    apiAccessKey: "",
+    apiBaseUrl: "",
+  },
+};
 
 export function init() {
-    if(!process.env.UNSPLASH_API_BASE_URL) throw `cannot find unsplash base url from env`
-    globalConfig.unsplash.apiBaseUrl = process.env.UNSPLASH_API_BASE_URL
-    
-    if(!process.env.UNSPLASH_ACCESS_KEY) throw `cannot find unsplash api access key from env`
-    globalConfig.unsplash.apiAccessKey = process.env.UNSPLASH_ACCESS_KEY
+  if (!process.env.UNSPLASH_API_BASE_URL)
+    throw `cannot find unsplash base url from env`;
+  globalConfig.unsplash.apiBaseUrl = process.env.UNSPLASH_API_BASE_URL;
+
+  if (!process.env.UNSPLASH_ACCESS_KEY)
+    throw `cannot find unsplash api access key from env`;
+  globalConfig.unsplash.apiAccessKey = process.env.UNSPLASH_ACCESS_KEY;
 }
