d6a5b9530bfc25dd190223d2dbfa6e08fa06cfc7
hanyiseo2 <shyshy81818181@gmail.com>
2023-09-21 19:09:14 +0900

    edit code for error handling

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import {
   Color,
   getRandomUnsplashImageUrl,
@@ -8,6 +8,24 @@ import {
 
 export async function getRandomImage(req: Request, res: Response) {
   const { width, height } = req.query;
+  if (width && height) {
+    if (isNaN(Number(width)) || isNaN(Number(height))) {
+      return res
+        .status(400)
+        .json("The value of width and height should be a number");
+    }
+    if (Number(width) > 2000 || Number(height) > 2000) {
+      return res
+        .status(400)
+        .json("The value of width and height should be smaller than 2000");
+    }
+    if (Number(width) <= 0 || Number(height) <= 0) {
+      return res
+        .status(400)
+        .json("The value of width and height should be bigger than 0");
+    }
+  }
+
   const randomImageUrl = await getRandomUnsplashImageUrl(
     Number(width),
     Number(height)
@@ -29,6 +47,8 @@ export async function getRandomSearchImage(req: Request, res: Response) {
 }
 
 export function createUrlWithParams(url: string, params: object) {
+  if (Object.keys(params).length === 0) return url;
+
   let paramsStr = "";
   const keys = Object.keys(params);
   for (let i = 0; i < keys.length; i++) {
@@ -38,6 +58,9 @@ export function createUrlWithParams(url: string, params: object) {
     if (i < keys.length - 1) {
       paramsStr += "&";
     }
+  }
+  if (url.includes("?")) {
+    return url + "&" + paramsStr;
   }
   return url + "?" + paramsStr;
 }
