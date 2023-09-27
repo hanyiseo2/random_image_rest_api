01d6a08ed007a79a05950f41995a14d7a0777dab
clo3olb <clo3olb@gmail.com>
2023-09-28 04:36:02 +0800

    add morgan in runtime

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import { getRandomImage, getRandomSearchImage } from "./handlers";
 import { ServerError } from "./error";
+import morgan from "morgan";
 
 import { init } from "./config";
 init();
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", getRandomImage);
 app.get("/:keyword", getRandomSearchImage);
