4b4c16456b5c701671b324057ce59f84d828eb8b
clo3olb <clo3olb@gmail.com>
2023-09-28 01:03:39 +0800

    recreate isNumberInRange function

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param url - The url to append the params to.
+ * @param params - The params to append to the url.
+ * @returns The url with the params appended.
+ */
 export function createUrlWithParams(url: string, params: object) {
   const keys = Object.keys(params);
   if (keys.length === 0) return url;
@@ -23,15 +29,15 @@ export function createUrlWithParams(url: string, params: object) {
   );
 }
 
-export function isNumberInRange(width: number, height: number) {
-  const max = 2000,
-    min = 0;
-  if (isNaN(Number(width)) || isNaN(Number(height)))
-    return "The value of width and height should be a number";
-  if (Number(width) > max || Number(height) > max)
-    return "The value of width and height should be smaller than 2000";
-  if (Number(width) <= min || Number(height) <= min)
-    return "The value of width and height should be bigger than 0";
+/**
+ * Checks if a number is within a specified range.
+ * @param n - The number to check.
+ * @param min - The minimum value of the range(inclusive).
+ * @param max - The maximum value of the range(inclusive).
+ * @returns A boolean indicating whether the number is within the range.
+ */
+export function isNumberInRange(n: number, min: number, max: number): boolean {
+  return n >= min && n <= max;
 }
 
 export function createWidthHeightObejct(width: number, height: number) {
