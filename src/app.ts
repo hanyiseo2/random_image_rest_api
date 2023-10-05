b60d1cf63a665b438d4f2f932cc37643d0ed13f3
hanyiseo2 <shyshy81818181@gmail.com>
2023-10-05 17:01:56 +0900

    detach swagger options

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,17 @@
-import express, { Request, Response, NextFunction } from "express";
 import { ClientError } from "./error";
+import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import swaggerUI from "swagger-ui-express";
-import swaggerJSDoc from "swagger-jsdoc";
 import { init } from "./config";
-import router from "./router";
-init();
+import { swaggerSpec } from "./swagger";
+import { getRandomImage, getRandomSearchImage } from "./handlers";
 
+init(); // initialize config(env vars)
 const app = express();
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Random-Image-API",
-      version: "1.0.0",
-      description: "creating random_image with query function",
-    },
-    basePath: "/",
-  },
-  apis: ["src/router.ts"],
-};
-
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
 app.use(morgan("dev"));
-app.use("/random", router);
+app.get("/random", getRandomImage);
+app.get("/search/:keyword", getRandomSearchImage);
 app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use((err: ClientError, req: Request, res: Response, next: NextFunction) => {
